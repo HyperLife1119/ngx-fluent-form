a132b82ec7509247582ff17bf23c1d18feec3d74
HyperLife1119 <HyperLife1119@users.noreply.github.com>
2022-07-29 03:50:40 +0000

    docs: change docs

--- fluent-form-basic-step-by-step-form-index-story-mdx.aaae44b9.iframe.bundle.js
+++ fluent-form-basic-step-by-step-form-index-story-mdx.aaae44b9.iframe.bundle.js
@@ -26,7 +26,7 @@
       date: Date.now()
     };
   }
-`},"./stories/fluent-form/basic/step-by-step-form/index.story.mdx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{__namedExportsOrder:()=>__namedExportsOrder,default:()=>index_story,stepByStepForm:()=>stepByStepForm});var react=__webpack_require__("./node_modules/react/index.js"),esm=__webpack_require__("./node_modules/@storybook/addon-docs/dist/esm/index.js"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js"),dist_esm=__webpack_require__("./node_modules/@mdx-js/react/dist/esm.js"),fluent_form=__webpack_require__("./stories/fluent-form/index.ts"),src=__webpack_require__("./projects/ngx-fluent-form/src/index.ts"),storybook=__webpack_require__("./stories/storybook.ts"),ts_dedent_esm=__webpack_require__("./node_modules/ts-dedent/esm/index.js");const story=(0,storybook.LS)({args:{schemas:(0,src.cr)((0,src.S6)().span(24).current(0).schemas((0,src.Nb)().title("第一步").schemas((0,src.qH)("input1InStep1").label("文本输入框").span(12),(0,src.qH)("input2InStep1").label("文本输入框").span(12)),(0,src.Nb)().title("第二步").schemas((0,src.qH)("inputInStep2").label("文本输入框"),(0,src.Wy)("switchInStep2").label("开关")),(0,src.Nb)().title("第三步").schemas((0,src.ru)("step3").span(24).schemas((0,src.J7)("textareaInStep3").label("文本域").span(24)),(0,src.LI)().subtype("submit").style("primary").content("提交").block(!0).span(24)))),model:{}}}),source=ts_dedent_esm.default`
+`},"./stories/fluent-form/basic/step-by-step-form/index.story.mdx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{__namedExportsOrder:()=>__namedExportsOrder,default:()=>index_story,stepByStepForm:()=>stepByStepForm});var react=__webpack_require__("./node_modules/react/index.js"),esm=__webpack_require__("./node_modules/@storybook/addon-docs/dist/esm/index.js"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js"),dist_esm=__webpack_require__("./node_modules/@mdx-js/react/dist/esm.js"),fluent_form=__webpack_require__("./stories/fluent-form/index.ts"),src=__webpack_require__("./projects/ngx-fluent-form/src/index.ts"),storybook=__webpack_require__("./stories/storybook.ts"),ts_dedent_esm=__webpack_require__("./node_modules/ts-dedent/esm/index.js");const story=(0,storybook.LS)({args:{schemas:(0,src.cr)((0,src.S6)().span(24).active(0).schemas((0,src.Nb)().title("第一步").schemas((0,src.qH)("input1InStep1").label("文本输入框").span(12),(0,src.qH)("input2InStep1").label("文本输入框").span(12)),(0,src.Nb)().title("第二步").schemas((0,src.qH)("inputInStep2").label("文本输入框"),(0,src.Wy)("switchInStep2").label("开关")),(0,src.Nb)().title("第三步").schemas((0,src.ru)("step3").span(24).schemas((0,src.J7)("textareaInStep3").label("文本域").span(24)),(0,src.LI)().subtype("submit").style("primary").content("提交").block(!0).span(24)))),model:{}}}),source=ts_dedent_esm.default`
   import { Component } from '@angular/core';
   import { button, form, group, input, step, steps, switcher, textarea } from 'ngx-fluent-form';
 
@@ -36,7 +36,7 @@
   })
   export class ExampleComponent {
     schemas: form(
-      steps().span(24).current(0).schemas(
+      steps().span(24).active(0).schemas(
         step().title('第一步').schemas(
           input('input1InStep1').label('文本输入框').span(12),
           input('input2InStep1').label('文本输入框').span(12),
@@ -49,7 +49,7 @@
           group('step3').span(24).schemas(
             textarea('textareaInStep3').label('文本域').span(24),
           ),
-          button().subtype('submit').subtype('primary').content('提交').block(true).span(24)
+          button().subtype('submit').style('primary').content('提交').block(true).span(24)
         )
       )
     );
@@ -57,4 +57,4 @@
     model = {};
   }
 `;function _createMdxContent(props){const _components=Object.assign({h1:"h1",p:"p",code:"code"},(0,dist_esm.ah)(),props.components);return(0,jsx_runtime.jsxs)(jsx_runtime.Fragment,{children:[(0,jsx_runtime.jsx)(esm.h_,{title:"FluentForm/Basic",component:fluent_form.$y.component,decorators:fluent_form.$y.decorators,argTypes:fluent_form.$y.argTypes,parameters:fluent_form.$y.parameters}),"\n",(0,jsx_runtime.jsx)(_components.h1,{children:"Step-by-Step Form - 分步表单"}),"\n",(0,jsx_runtime.jsxs)(_components.p,{children:["如果表单控件数量繁多，我们可能希望将表单拆分为几个步骤以改善用户体验，这时候可以组合使用 ",(0,jsx_runtime.jsx)(_components.code,{children:"steps"})," 和 ",(0,jsx_runtime.jsx)(_components.code,{children:"step"})," 组件来构建分步表单："]}),"\n",(0,jsx_runtime.jsx)(esm.Xz,{withToolbar:!0,isExpanded:!0,mdxSource:source,children:(0,jsx_runtime.jsx)(esm.oG,{name:"Step-by-Step Form",children:story})})]})}function MDXContent(props={}){const{wrapper:MDXLayout}=Object.assign({},(0,dist_esm.ah)(),props.components);return MDXLayout?(0,jsx_runtime.jsx)(MDXLayout,Object.assign({},props,{children:(0,jsx_runtime.jsx)(_createMdxContent,props)})):_createMdxContent(props)}const stepByStepForm=(0,esm.Xb)(story);stepByStepForm.storyName="Step-by-Step Form",stepByStepForm.parameters={storySource:{source:"story"}};const componentMeta={title:"FluentForm/Basic",parameters:fluent_form.$y.parameters,decorators:fluent_form.$y.decorators,component:fluent_form.$y.component,argTypes:fluent_form.$y.argTypes,includeStories:["stepByStepForm"]},mdxStoryNameToKey={"Step-by-Step Form":"stepByStepForm"};componentMeta.parameters=componentMeta.parameters||{},componentMeta.parameters.docs={...componentMeta.parameters.docs||{},page:()=>react.createElement(esm.aT,{mdxStoryNameToKey,mdxComponentAnnotations:componentMeta},react.createElement(MDXContent,null))};const index_story=componentMeta,__namedExportsOrder=["stepByStepForm"]},"./stories/fluent-form/index.ts.css?ngResource!=!./node_modules/@ngtools/webpack/src/loaders/inline-resource.js?data=CiAgICBwcmUgeyBwYWRkaW5nOiA1cHg7IGJhY2tncm91bmQ6ICNmNWY1ZjU7IGJvcmRlcjogMXB4IHNvbGlkICNlMGUwZTAgfQogIA%3D%3D!./stories/fluent-form/index.ts":module=>{module.exports="\n    pre { padding: 5px; background: #f5f5f5; border: 1px solid #e0e0e0 }\n  "}}]);
-//# sourceMappingURL=fluent-form-basic-step-by-step-form-index-story-mdx.ae57bfab.iframe.bundle.js.map
+//# sourceMappingURL=fluent-form-basic-step-by-step-form-index-story-mdx.aaae44b9.iframe.bundle.js.map
---
+++ fluent-form-basic-tabs-form-index-story-mdx.8361fefb.iframe.bundle.js
@@ -0,0 +1,58 @@
+"use strict";(self.webpackChunkngx_fluent_form=self.webpackChunkngx_fluent_form||[]).push([[169],{"./stories/fluent-form/index.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{$y:()=>meta,fO:()=>moduleSource,hb:()=>cmpSource});var tslib__WEBPACK_IMPORTED_MODULE_2__=__webpack_require__("./node_modules/tslib/tslib.es6.js"),_home_runner_work_ngx_fluent_form_ngx_fluent_form_stories_fluent_form_index_ts_css_ngResource_home_runner_work_ngx_fluent_form_ngx_fluent_form_node_modules_ngtools_webpack_src_loaders_inline_resource_js_data_CiAgICBwcmUgeyBwYWRkaW5nOiA1cHg7IGJhY2tncm91bmQ6ICNmNWY1ZjU7IGJvcmRlcjogMXB4IHNvbGlkICNlMGUwZTAgfQogIA_3D_3D_home_runner_work_ngx_fluent_form_ngx_fluent_form_stories_fluent_form_index_ts__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./stories/fluent-form/index.ts.css?ngResource!=!./node_modules/@ngtools/webpack/src/loaders/inline-resource.js?data=CiAgICBwcmUgeyBwYWRkaW5nOiA1cHg7IGJhY2tncm91bmQ6ICNmNWY1ZjU7IGJvcmRlcjogMXB4IHNvbGlkICNlMGUwZTAgfQogIA%3D%3D!./stories/fluent-form/index.ts"),_angular_core__WEBPACK_IMPORTED_MODULE_3__=__webpack_require__("./node_modules/@angular/core/fesm2020/core.mjs"),stories_storybook__WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./stories/storybook.ts"),ts_dedent__WEBPACK_IMPORTED_MODULE_4__=__webpack_require__("./node_modules/ts-dedent/esm/index.js");let FluentFormWrapperComponent=class FluentFormWrapperComponent extends stories_storybook__WEBPACK_IMPORTED_MODULE_1__.zW{};FluentFormWrapperComponent=(0,tslib__WEBPACK_IMPORTED_MODULE_2__.gn)([(0,_angular_core__WEBPACK_IMPORTED_MODULE_3__.Component)({selector:"fluent-form-wrapper",template:'\n    <div nz-row nzGutter="20">\n      <fluent-form\n        nz-col\n        nzFlex="2"\n        [schemas]="schemas"\n        [(model)]="model"\n        [layout]="layout"\n        [colon]="colon"\n        [spinning]="spinning"\n        [spinTip]="spinTip"\n        [spinSize]="spinSize"></fluent-form>\n      <div nz-col nzFlex="1">\n        <pre>{{ model | json }}</pre>\n      </div>\n    </div>\n  ',styles:[_home_runner_work_ngx_fluent_form_ngx_fluent_form_stories_fluent_form_index_ts_css_ngResource_home_runner_work_ngx_fluent_form_ngx_fluent_form_node_modules_ngtools_webpack_src_loaders_inline_resource_js_data_CiAgICBwcmUgeyBwYWRkaW5nOiA1cHg7IGJhY2tncm91bmQ6ICNmNWY1ZjU7IGJvcmRlcjogMXB4IHNvbGlkICNlMGUwZTAgfQogIA_3D_3D_home_runner_work_ngx_fluent_form_ngx_fluent_form_stories_fluent_form_index_ts__WEBPACK_IMPORTED_MODULE_0__]})],FluentFormWrapperComponent);const meta=(0,stories_storybook__WEBPACK_IMPORTED_MODULE_1__.lO)({component:FluentFormWrapperComponent}),moduleSource=ts_dedent__WEBPACK_IMPORTED_MODULE_4__.default`
+  import { FluentFormModule } from 'ngx-fluent-form';
+
+  @NgModule({
+    imports: [
+      FluentFormModule
+    ]
+  })
+  export class YourModule { }
+`,cmpSource=ts_dedent__WEBPACK_IMPORTED_MODULE_4__.default`
+  import { date, form, number, text } from 'ngx-fluent-form';
+
+  @Component({
+    template: \`<fluent-form [(model)]="model" [schemas]="schemas"></fluent-form>\`
+  })
+  export class Component {
+    schemas = form(
+      text('text').label('文本').span(6),
+      number('number').label('数字').span(3).max(100),
+      date('date').label('日期').span(6)
+    );
+
+    model = {
+      text: 'fluent-form',
+      number: 10,
+      date: Date.now()
+    };
+  }
+`},"./stories/fluent-form/basic/tabs-form/index.story.mdx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{__namedExportsOrder:()=>__namedExportsOrder,default:()=>index_story,tabsForm:()=>tabsForm});var react=__webpack_require__("./node_modules/react/index.js"),esm=__webpack_require__("./node_modules/@storybook/addon-docs/dist/esm/index.js"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js"),dist_esm=__webpack_require__("./node_modules/@mdx-js/react/dist/esm.js"),fluent_form=__webpack_require__("./stories/fluent-form/index.ts"),src=__webpack_require__("./projects/ngx-fluent-form/src/index.ts"),storybook=__webpack_require__("./stories/storybook.ts"),ts_dedent_esm=__webpack_require__("./node_modules/ts-dedent/esm/index.js");const story=(0,storybook.LS)({args:{schemas:(0,src.cr)((0,src.$3)().span(24).schemas((0,src.n2)().title("用户名").schemas((0,src.qH)("username").label("用户名").span(24),(0,src.qH)("password").subtype("password").label("密码").span(24),(0,src.MJ)("remember").content("记住我"),(0,src.LI)().subtype("submit").style("primary").content("立即登录").block(!0).span(24)),(0,src.n2)().title("手机号").schemas((0,src.qH)("cellphone").subtype("tel").label("手机号").span(24),(0,src.qH)("password").subtype("password").label("密码").span(24),(0,src.MJ)("remember").content("记住我"),(0,src.LI)().subtype("submit").style("primary").content("立即登录").block(!0).span(24)))),model:{}}}),source=ts_dedent_esm.default`
+  import { Component } from '@angular/core';
+  import { button, checkbox, form, input, tab, tabset } from 'ngx-fluent-form';
+
+  @Component({
+    selector: 'example-component',
+    template: \`<fluent-form [schemas]="schemas" [(model)]="model"></fluent-form>\`
+  })
+  export class ExampleComponent {
+    schemas: form(
+      tabset().span(24).schemas(
+        tab().title('用户名').schemas(
+          input('username').label('用户名').span(24),
+          input('password').subtype('password').label('密码').span(24),
+          checkbox('remember').content('记住我'),
+          button().subtype('submit').style('primary').content('立即登录').block(true).span(24)
+        ),
+        tab().title('手机号').schemas(
+          input('cellphone').subtype('tel').label('手机号').span(24),
+          input('password').subtype('password').label('密码').span(24),
+          checkbox('remember').content('记住我'),
+          button().subtype('submit').style('primary').content('立即登录').block(true).span(24)
+        ),
+      )
+    );
+
+    model = {};
+  }
+`;function _createMdxContent(props){const _components=Object.assign({h1:"h1",p:"p",code:"code"},(0,dist_esm.ah)(),props.components);return(0,jsx_runtime.jsxs)(jsx_runtime.Fragment,{children:[(0,jsx_runtime.jsx)(esm.h_,{title:"FluentForm/Basic",component:fluent_form.$y.component,decorators:fluent_form.$y.decorators,argTypes:fluent_form.$y.argTypes,parameters:fluent_form.$y.parameters}),"\n",(0,jsx_runtime.jsx)(_components.h1,{children:"Tabs Form - 标签页表单"}),"\n",(0,jsx_runtime.jsxs)(_components.p,{children:["标签页表单适合在同一区域内切换不同的表单，可以组合使用 ",(0,jsx_runtime.jsx)(_components.code,{children:"tabset"})," 和 ",(0,jsx_runtime.jsx)(_components.code,{children:"tab"})," 组件来构建标签页表单："]}),"\n",(0,jsx_runtime.jsx)(esm.Xz,{withToolbar:!0,isExpanded:!0,mdxSource:source,children:(0,jsx_runtime.jsx)(esm.oG,{name:"Tabs Form",children:story})})]})}function MDXContent(props={}){const{wrapper:MDXLayout}=Object.assign({},(0,dist_esm.ah)(),props.components);return MDXLayout?(0,jsx_runtime.jsx)(MDXLayout,Object.assign({},props,{children:(0,jsx_runtime.jsx)(_createMdxContent,props)})):_createMdxContent(props)}const tabsForm=(0,esm.Xb)(story);tabsForm.storyName="Tabs Form",tabsForm.parameters={storySource:{source:"story"}};const componentMeta={title:"FluentForm/Basic",parameters:fluent_form.$y.parameters,decorators:fluent_form.$y.decorators,component:fluent_form.$y.component,argTypes:fluent_form.$y.argTypes,includeStories:["tabsForm"]},mdxStoryNameToKey={"Tabs Form":"tabsForm"};componentMeta.parameters=componentMeta.parameters||{},componentMeta.parameters.docs={...componentMeta.parameters.docs||{},page:()=>react.createElement(esm.aT,{mdxStoryNameToKey,mdxComponentAnnotations:componentMeta},react.createElement(MDXContent,null))};const index_story=componentMeta,__namedExportsOrder=["tabsForm"]},"./stories/fluent-form/index.ts.css?ngResource!=!./node_modules/@ngtools/webpack/src/loaders/inline-resource.js?data=CiAgICBwcmUgeyBwYWRkaW5nOiA1cHg7IGJhY2tncm91bmQ6ICNmNWY1ZjU7IGJvcmRlcjogMXB4IHNvbGlkICNlMGUwZTAgfQogIA%3D%3D!./stories/fluent-form/index.ts":module=>{module.exports="\n    pre { padding: 5px; background: #f5f5f5; border: 1px solid #e0e0e0 }\n  "}}]);
+//# sourceMappingURL=fluent-form-basic-tabs-form-index-story-mdx.8361fefb.iframe.bundle.js.map
